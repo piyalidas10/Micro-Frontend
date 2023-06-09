2b8dc12d20a03b536507578f1a8508f53e14db94
piyalidas10 <piyalidas.it@gmail.com>
2023-06-09 10:07:00 +0530

    multi repo mfe

---
+++ MFE_Tutorial/multi-workspace/host-app/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const MFE_APP_URL = "http://localhost:4300/remoteEntry.js";
+const MFE_QUIZ_APP_URL = "http://localhost:4400/remoteEntry.js";
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {
+    path: 'todo-list',
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: MFE_APP_URL,
+        remoteName: "mfeApp",
+        exposedModule: "./TodoListModule"
+      }).then(m => m.TodoListModule).catch(err => console.log(err));
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routeCompArr = [HomeComponent];
